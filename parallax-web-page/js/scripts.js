3be26c0e654edb9844445f9f6f0acc463aff04b6
zali <betzalelperlow@gmail.com>
2018-07-01 00:06:49 -0400

    adding parallax scrolling effects

---
+++ parallax-web-page/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(window).bind("scroll", function(e) {
+    parallax();
+  });
+});
+
+function parallax() {
+  var scrollPosition = $(window).scrollTop();
+  $("#stars").css("top", 0 - scrollPosition * 0.2 + "px");
+  $("#images").css("top", 0 - scrollPosition * 0.5 + "px");
+}
